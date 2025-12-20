ebdee06caeddf3005f3e6e4dd7327951a01a5e0c
imagicity-stack <imagicityart@gmail.com>
2025-12-20 23:58:03 +0530

    Adopt square corners sitewide

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
     "./app/**/*.{js,jsx}"
   ],
   theme: {
+    borderRadius: {
+      none: "0px",
+      sm: "0px",
+      DEFAULT: "0px",
+      md: "0px",
+      lg: "0px",
+      xl: "0px",
+      "2xl": "0px",
+      "3xl": "0px",
+      full: "0px"
+    },
     extend: {
       colors: {
         cardinal: "#A50021",
