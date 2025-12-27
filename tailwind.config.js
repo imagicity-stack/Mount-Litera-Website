88e782af092144bb2835f07779b53b0b12b3cafd
imagicity-stack <imagicityart@gmail.com>
2025-12-27 11:38:10 +0530

    Apply new color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,10 +19,11 @@ module.exports = {
     },
     extend: {
       colors: {
-        cardinal: "#A50021",
-        gold: "#C3A572",
-        parchment: "#F7F2E9",
-        midnight: "#0F172A"
+        cardinal: "#890304",
+        gold: "#F8F2BF",
+        parchment: "#E8E5C3",
+        midnight: "#00113A",
+        cobalt: "#002263"
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
