380991a9af01934277a8a00e86775ff3a1342929
imagicity-stack <imagicityart@gmail.com>
2026-03-09 12:20:16 +0530

    Slow success meter analysis ticks and stagger result reveals

---
+++ pages/successmeter.js
@@ -0,0 +1,11 @@
+import Seo from '@/components/Seo';
+import SuccessMeterPage from '@/components/SuccessMeterPage';
+
+export default function SuccessMeterRoute() {
+  return (
+    <>
+      <Seo path="/successmeter" />
+      <SuccessMeterPage />
+    </>
+  );
+}
